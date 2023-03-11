a92cf239ce75d712d878a72d6900bf722d2f4941
George Nakas <george.nakas@paul-digital.de>
2023-03-11 19:44:38 +0100

    start in bootup

--- app/src/main/java/io/github/controlwear/joystickdemo/BackgroundHC05.java
+++ app/src/main/java/io/github/controlwear/joystickdemo/BackgroundHC05.java
@@ -1,13 +1,30 @@
 package io.github.controlwear.joystickdemo;
 
+import android.bluetooth.BluetoothSocket;
 import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 public class BackgroundHC05 implements Runnable {
     Thread backgroundThread;
     private int m_speed;
     private int m_steering;
 
-    public void start() {
+    private OutputStream m_outStream;
+
+    public void start(BluetoothSocket p_btSocket) {
+        if(m_outStream == null) {
+            try {
+                if (p_btSocket != null){
+                    m_outStream = p_btSocket.getOutputStream();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         if( backgroundThread == null ) {
             backgroundThread = new Thread( this );
             backgroundThread.start();
@@ -15,6 +32,9 @@ public class BackgroundHC05 implements Runnable {
     }
 
     public void stop() {
+        if(m_outStream != null) {
+            m_outStream = null;
+        }
         if( backgroundThread != null ) {
             backgroundThread.interrupt();
         }
@@ -36,15 +56,29 @@ public class BackgroundHC05 implements Runnable {
         return new String(hexChars);
     }
     public void sendHC05Packet(){
-        byte[] frame= {(byte)205,(byte)171,0,0,0,0,0,0};
-        frame[2] = (byte) (m_steering & 0xff);
-        frame[3] = (byte) ((m_steering & 0xffff) >> 8);
-        frame[4] = (byte) (m_speed & 0xff);
-        frame[5] = (byte) ((m_speed & 0xffff) >> 8);
-        short crc = (short)(frame[0]^frame[1]^frame[2]^frame[3]^frame[4]^frame[5]^frame[6]^frame[7]);
-        frame[6] = (byte) (crc & 0xff);
-        frame[7] = (byte) ((crc & 0xffff) >> 8);
+
+        byte[] frame= {(byte)205,(byte)171,(byte)(m_steering & 0xff),(byte)((m_steering & 0xffff) >> 8),(byte)(m_speed & 0xff),(byte)((m_speed & 0xffff) >> 8),(byte)0,(byte)0};
+
+        int frame_uint16 = BitUtils.uint16((byte)205,(byte)171);
+
+        int steering_uint16 = BitUtils.uint16((byte)(m_steering & 0xff),(byte)((m_steering & 0xffff) >> 8));
+        int speed_uint16 = BitUtils.uint16((byte)(m_speed & 0xff),(byte)((m_speed & 0xffff) >> 8));
+
+        int crc = frame_uint16 ^ steering_uint16 ^ speed_uint16;
+//        Log.i("crc :",""+crc);
+        frame[6] = (byte) ((crc & 0xffff) >> 8);
+        frame[7] = (byte) (crc & 0xff);
+
         Log.i("TAG",bytesToHex(frame));
+
+//        byte[] frame= {(byte)205,(byte)171,(byte)00,(byte)00,(byte)255,(byte)00,(byte)50,(byte)171};
+        if (m_outStream != null){
+            try {
+                m_outStream.write(frame);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 
--- app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
+++ app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
@@ -1,10 +1,20 @@
 package io.github.controlwear.joystickdemo;
 
+import static android.support.v4.content.ContextCompat.startActivity;
+
 import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.UUID;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
@@ -19,12 +29,38 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextViewCoordinateRight;
     private BackgroundHC05 controller ;
 
+    private BluetoothSocket m_btSocket;
+    public BluetoothAdapter btAdapter;
+    public BluetoothDevice btDevice;
+    public BluetoothSocket btSocket;
+
+    public static final String SERVICE_ID = "00001101-0000-1000-8000-00805f9b34fb"; //SPP UUID
+    public static final String SERVICE_ADDRESS = "98:D3:61:F6:C8:FC"; // HC-05 BT ADDRESS
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        controller= new BackgroundHC05();
-        controller.start();
+
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        btDevice = btAdapter.getRemoteDevice(SERVICE_ADDRESS);
+        if(btAdapter == null) {
+            Log.e("TAG", "Bluetooth not available");
+        } else {
+            if(!btAdapter.isEnabled()) {
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent,3);
+            } else {
+                ConnectThread connectThread = new ConnectThread(btDevice);
+                connectThread.start();
+            }
+        }
+
+
+        controller = new BackgroundHC05();
+//        controller.start(null);
         mTextViewAngleLeft = (TextView) findViewById(R.id.textView_angle_left);
         mTextViewStrengthLeft = (TextView) findViewById(R.id.textView_strength_left);
 
@@ -79,5 +115,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop(){
         super.onStop();
         controller.stop();
+    }
+
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket thisSocket;
+        private final BluetoothDevice thisDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            BluetoothSocket tmp = null;
+            thisDevice = device;
+
+            try {
+                tmp = thisDevice.createRfcommSocketToServiceRecord(UUID.fromString(SERVICE_ID));
+            } catch (IOException e) {
+                Log.e("TEST", "Can't connect to service");
+            }
+            thisSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            btAdapter.cancelDiscovery();
+
+            try {
+                thisSocket.connect();
+                Log.d("TESTING", "Connected to shit");
+            } catch (IOException connectException) {
+                try {
+                    thisSocket.close();
+                } catch (IOException closeException) {
+                    Log.e("TEST", "Can't close socket");
+                }
+                return;
+            }
+
+            btSocket = thisSocket;
+            controller.start(btSocket);
+        }
+        public void cancel() {
+            try {
+                controller.stop();
+                thisSocket.close();
+            } catch (IOException e) {
+                Log.e("TEST", "Can't close socket");
+            }
+        }
     }
 }
